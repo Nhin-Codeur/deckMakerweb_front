292efb1942b2ac92132edfd6f620ce924f1a6241
Elliot <clemenceauelliot@gmail.com>
2022-11-13 19:20:24 +0100

    ajout des decks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,52 @@ export class AppComponent {
   current_card:string = ""
   all_results = []
   deck : Array<any> = []
+  current_deck_name : string = ""
+  list_of_decks : Array<any> = []
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) {
+    this.afficherDecks()
+  }
+
+  private affiche_resultat_requete(response:any){
+    console.log(response)
+  }
+
+  
+
+  private get_occurence(tab:any, id:number) {
+    var i = 0
+    tab.forEach((card:any) => {
+      if (card['id'] == id ){
+        i++
+      }
+    })
+    return i
+
+  } 
+
 
 
   public addCard(card:any) {
-    this.deck.push(card)
-    console.log(this.deck)
+    if(this.get_occurence(this.deck, card['id']) < 2 && this.deck.length < 30 ) {
+
+      this.deck.push(card)
+      
+    }
+  }
+
+  public saveDeck() {
+    console.log(this.deck.map(x => x["id"]))
+    this.apiService.saveDeck(this.deck.map(x => x["id"]), this.current_deck_name).subscribe(this.affiche_resultat_requete)
   }
 
   public searchCard() {
     this.apiService.getCard(this.current_card).subscribe((response: any) => this.all_results = response.cards);
   }
+
+  public afficherDecks() {
+    this.apiService.getDecks().subscribe((response: any) => console.log(response))
+
+  }
+
 } 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Deck } from '../types/deck.model';
 
-@Injectable({
+@Injectable({ 
   providedIn: 'root'
 })
 export class ApiService {
 
-  private url = "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=EU8PvqqI67mSJUWTTeYrKiGHxNxJldF0qE&textFilter=";
+  private url_hearthstone_api = "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token=EUQfk7Y9q0ZqRMjs9POrQmYFZBNNEkULin&textFilter=";
+  private ip_rafael = "http://78.203.42.218:27017"
+  private ip_locale = "/api"
 
   constructor(private http: HttpClient) { 
   }
 
-
+  public saveDeck(deck:Array<number>, current_deck_name:string) {
+    const options = {headers: {'Content-Type' : 'application/json'}}
+    return this.http.post<Deck>(this.ip_locale + "/deck" + "/" + current_deck_name, JSON.stringify(deck),options)
+    
+  }
   
+
 
 
 
   public getCard(card_name:string){
     card_name = card_name.replace(' ','%20')
-    let current_url = this.url + card_name
+    let current_url = this.url_hearthstone_api + card_name
     return this.http.get(current_url)
 
     
+  }
+
+  public getDecks(){
+    return this.http.get(this.ip_locale + "/decks")
   }
 }
